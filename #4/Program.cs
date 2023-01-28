c9563758f501fa46500f98187bc50111b919b76c
Centurion195 <Vadim-0211@yandex.ru>
2023-01-28 17:20:27 +0300

    Task #4 completed

---
+++ #4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine ("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a%2==0)
+Console.WriteLine ("Введенное число четное!");
+else
+Console.WriteLine ("Введенное число нечетное!");
