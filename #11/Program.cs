29a7b219ec1530990ce1bd499e3be5d7d52f1d2f
Centurion195 <Vadim-0211@yandex.ru>
2023-01-28 18:36:59 +0300

    Task #11 completed

---
+++ #11/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine ("Введите число: ");
+string num = Console.ReadLine();
+
+if (num.Length>2)
+Console.WriteLine ("Третья цифра числа: "+ num.Substring(2,1));
+else
+Console.WriteLine ("Нет третьей цифры числа!");
