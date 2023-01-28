f9702a71ac654b548cd5269ac1a7e78c6f70593d
Centurion195 <Vadim-0211@yandex.ru>
2023-01-28 18:03:57 +0300

    Task #8 completed

---
+++ #8/Program.cs
@@ -0,0 +1,11 @@
+﻿string num = new Random().Next(10, 100).ToString();
+
+string max = null;
+
+if (Convert.ToInt32(num.Substring(0,1))>Convert.ToInt32(num.Substring(1,1)))
+max = num.Substring(0,1);
+else
+max = num.Substring(1,1);
+
+Console.WriteLine ("Случайное число: " + num);
+Console.WriteLine ("Наибольшая цифра числа: " + max);
