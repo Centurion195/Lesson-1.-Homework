375e1dc13190366549083fbf89356909540e2c0e
Centurion195 <Vadim-0211@yandex.ru>
2023-01-28 17:45:38 +0300

    Task #7 completed

---
+++ #7/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите трехзначное число: ");
+string a = Console.ReadLine();
+
+if (a.Length == 3)
+{
+    string num3 = a.Substring(1,1);
+    Console.WriteLine ("Вторая цифра числа: " + num3);
+}
+else
+{
+    Console.WriteLine ("Введено не трехзначное число!");
+}
