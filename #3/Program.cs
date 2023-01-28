b178b595eea32581884f7786a39672c1d1e825bd
Centurion195 <Vadim-0211@yandex.ru>
2023-01-28 17:10:18 +0300

    Task #3 completed

---
+++ #3/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] numbers = new int[3];
+Console.WriteLine ("Введите 3 числа: ");
+
+for (int i = 0; i<numbers.Length; i++)
+{
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int max = numbers[0];
+
+for (int i = 0; i<numbers.Length; i++)
+{
+    if (numbers[i]>max) max = numbers[i];
+}
+
+Console.WriteLine ("Максимальное число: " + max);
