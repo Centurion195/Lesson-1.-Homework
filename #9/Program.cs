11cb20e9669855179f81f2b6baab005a42948d43
Centurion195 <Vadim-0211@yandex.ru>
2023-01-28 18:15:33 +0300

    Task #9 completed

---
+++ #9/Program.cs
@@ -0,0 +1,4 @@
+﻿string num = new Random().Next(100,1000).ToString();
+Console.WriteLine ("Случайное трехзначное число: " + num);
+string NewNum = num.Replace(num.Substring(1,1), "");
+Console.WriteLine ("Удалена вторая цифра числа: " + NewNum);
