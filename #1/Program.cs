6b88d7157345c43f35b05c9ce0b26e57e8e18b82
Centurion195 <Vadim-0211@yandex.ru>
2023-01-28 16:35:11 +0300

    Complete task #1

--- #1/Program.cs
+++ #1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Task1 analys = new Task1();
+Console.WriteLine("Введите два числа: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+bool res = analys.Calc(a,b);
+if (res==true)
+    Console.WriteLine("Первое число является квадратом второго.");
+    else
+    Console.WriteLine("Первое число НЕ является квадратом второго.");
+
+class Task1
+    {
+        public bool Calc (int x, int y)
+        {
+            bool result = false;
+            if (x==(y*y)) result = true;
+            return result;
+        }
+    }
