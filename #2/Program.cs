959c467a707304d3155dabc36b0c05779047f46e
Centurion195 <Vadim-0211@yandex.ru>
2023-01-28 16:50:33 +0300

    Task #2 comleted

---
+++ #2/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] DayOfWeek = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+Console.WriteLine("Введите номер недели: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a<=DayOfWeek.Length)
+Console.WriteLine(DayOfWeek[a-1]);
+else
+Console.WriteLine("Такого дня недели нет!");
