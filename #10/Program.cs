a8d555f0d7a2fd6c3a5054dcc3c8e75d2b49882a
Centurion195 <Vadim-0211@yandex.ru>
2023-01-28 18:23:14 +0300

    Task #10 completed

---
+++ #10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine ("Введите два числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1%num2==0)
+Console.WriteLine ("Первое число кратно второму.");
+else
+Console.WriteLine ("Первое число не кратно второму. Остаток: " + num1%num2);
